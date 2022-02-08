61848c27d7e55cc6f8738e8dcb23bf754ebcb6e2
Rnoder <99229023+Rnoder@users.noreply.github.com>
2022-02-08 11:34:06 +0900

    Add files via upload

---
+++ Blender_import.py
@@ -0,0 +1,18 @@
+import numpy as np
+import os
+import bpy
+import glob
+
+os.chdir(rf'C:\Users\endeavor1\Desktop\earth\Blender')
+list = glob.glob('*.txt')
+N = len(list)
+
+for i in range(0,N):
+    text = list[i]
+    verts = np.loadtxt(text)
+    mesh = bpy.data.meshes.new('Planet')
+    ob = bpy.data.objects.new('earth_'+str(i), mesh)
+    bpy.context.scene.collection.objects.link(ob)
+    faces = []
+    mesh.from_pydata(verts, [], faces)
+    mesh.update()
